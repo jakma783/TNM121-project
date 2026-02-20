9894bf90026f44a50fbf8f573dab53b6385ebfcd
jakma783 <jakma783@student.liu.se>
2026-02-20 11:33:07 +0100

    these two folders got added to remove "Web Projek"

---
+++ client/chat/chat.js
@@ -0,0 +1,133 @@
+const allGenres = ["action", "drama", "comedy"]; // make fetching function to extract all genre names
+const dontWords = ["no", "dont", "don't", "hate", "Dont", "without", "remove", "disslike"];
+
+const userGenre = [];
+const userDisslikeGenres = [];
+
+let chatLine = 1;
+let userLine = 2;
+
+function sendTextBubble(userMessage) {
+     userLine = userLine + 1;
+     const textBubble = document.createElement('div');
+     textBubble.className = "user-message";
+
+     const userText = document.createElement('p');
+     userText.textContent = userMessage;
+     userText.id = "message-sent";
+
+     textBubble.style.gridRow = userLine;
+
+     textBubble.appendChild(userText);
+     document.body.appendChild(textBubble);
+
+     console.log("User complete message was: " + userMessage);
+}
+
+
+
+
+//send button-----------------------------------------------------------------------------------------------------
+function sendPrompt() {
+     console.log("Sending-button was clicked");
+     const userMessage = document.getElementById('user-writer-bar').value;
+     sendTextBubble(userMessage);
+     mainProcessMessage();
+     console.log("User genre is: " + userGenre);
+     return userMessage;
+}
+
+
+
+//main process function--------------------------------------------------------------------------------
+function mainProcessMessage() {
+     const messageArray = splitMessage(); // user message being split
+     disslike(messageArray);
+     return getGenre(messageArray);
+}
+
+
+
+
+//split message functions-----------------------------------------------------------------------------------------
+function splitMessage() {
+     const fullString = document.getElementById('message-sent');
+     const messageArray = fullString.innerText.split(" ");
+
+     for (let i = 0; i < messageArray.length; ++i) {
+          console.log("word " + i + ": " + messageArray[i]);
+     }
+     return messageArray;
+}
+
+//genre functions-------------------------------------------------------------------------------------------------
+function getGenre(messageArray) {
+
+     for (let i = 0; i < messageArray.length; ++i) {
+          const thisWord = (isGenre(messageArray[i]));
+
+          if (thisWord != null) {
+               userGenre.push(thisWord);
+               console.log("currently, userGenre is: " + userGenre);
+          }
+     }
+
+     return userGenre;
+}
+
+function isGenre(word) {
+     for (let i = 0; i < allGenres.length; ++i) {
+          if (allGenres[i] === word) {
+               console.log("The word: " + word + " is a genre!");
+               console.log("your genre is: " + allGenres[i]);
+               return allGenres[i];
+          }
+     }
+     console.log("the word: " + word + "  - is not a genre");
+     return null;
+}
+
+//Remover function (Dont's)
+function disslike(messageWords) { // void function - no return value - only removes objects if dont's occur
+
+     for (let i = 0; i < messageWords.length; ++i) { //go through each word in message
+
+          for (let j = 0; j < dontWords.length; ++j) { //check if it matches any of the "dont's" words
+
+               if (messageWords[i] === dontWords[j]) {
+                    console.log("DONT word occured: " + messageWords[i]);
+
+                    for (let r= i+1; r < messageWords.length; ++r) { //start from the dont word and remove all other categories after that
+                         searchWord(messageWords[r]);
+                    }
+                    
+               }
+
+          }
+     }
+
+
+}
+
+function searchWord(potentialWord) { // one for loop for each user Category Array (top of document)
+
+     for (let i = 0; i < userGenre.length; ++i) {
+          if (potentialWord === userGenre[i]) {
+               //remove that word from userGenre Array
+               //add the genre to userDisslikeGenres;
+               console.log("the word: " + potentialWord + " is a genre")
+               return true
+          }
+     }
+
+     return false
+}
+
+
+
+
+
+
+
+
+
---
+++ client/genre.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", ( )=> {
+    const grid = document.getElementById("genreGrid");
+    if (!grid) return;
+
+    const genres = ["Sci-Fi", "Thriller", "Action", "Romance", "Comedy", "Horror", "Mystery", "Fantasy","Adventure", "Crime", "Documentary", 
+         "Biography", "Supernatural", "Coming-of-Age", "Animation", "War", "Family", "Music", "Slice of Life", "Historical Drama",
+         "Psychological Thriller", "Action", "Fantasy", "Mystery", "Drama", "Horror", "Thriller", "Romantic Comedy"]; // queryFiltering by genre (fetch)
+
+    genres.sort();
+
+    for (const genre of genres) {
+        const div = document.createElement("div");
+        div.className = "genre-item";
+        div.textContent = genre;
+        grid.appendChild(div);
+    }  
+});
