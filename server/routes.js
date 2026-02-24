d68bdbaaf09d8012368d4c063330b4cfe5d626d5
AlexSeljimi <alexselimi3@gmail.com>
2026-02-24 14:13:40 +0100

    Add files via upload

--- server/routes.js
+++ server/routes.js
@@ -4,9 +4,18 @@ const path = require("node:path");
  
 async function handleRequest(req, res, db) {
  
-    const parsedUrl = url.parse(req.url, true);
+    const parsedUrl = new URL(req.url, `http://${req.headers.host}`);
     const pathname = parsedUrl.pathname;
- 
+
+    
+    
+    
+    /* KANSKE TA BORT*/
+    //const parsedUrl = url.parse(req.url, true);
+    //const pathname = parsedUrl.pathname;
+    /*-------------------------------------*/
+
+
     // To get all data on movies with a limit
     if (req.method === "GET" && pathname === "/movies") {
  
@@ -52,6 +61,7 @@ async function handleRequest(req, res, db) {
             {
                 $project: {
                     _id: 1,
+                    normalized_id: 1,
                     title: "$name",
                     year: 1,
                     genre: 1,
@@ -86,8 +96,8 @@ async function handleRequest(req, res, db) {
     if(req.method === "GET" && pathname.startsWith("/image/")) {
         const normalizedID = pathname.split("/")[2];
 
-        const imagePath = path.join(__dirname, "../client/media", normalizedID + ".png");
-        const placeholderPath = path.join(__dirname, "../client/media", "placeholder.png");
+        const imagePath = path.join(__dirname, "../../client/media", normalizedID + ".png");
+        const placeholderPath = path.join(__dirname, "../../client/media", "placeholder.png");
 
         fs.readFile(imagePath, (err, data) => {
             // If there is no image to given normalized Id
@@ -105,6 +115,38 @@ async function handleRequest(req, res, db) {
             }
         });
         return;
+    }
+
+    if(req.method === "GET" && pathname === "/search"){
+        const query = parsedUrl.searchParams.get("query");
+
+        if(!query || query.length < 3){
+            res.statusCode = 400;
+            return res.end("Query must be at least 3 characters");
+        }
+
+        const escaped = query.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+        const prefixRegex = new RegExp("^" + escaped, "i");
+
+        const movies = await db.collection("imdb").find(
+            {
+                name: {
+                    $regex: prefixRegex,
+                }
+            },
+            {
+                projection: {
+                    _id: 1,
+                    normalized_id: 1,
+                    name: 1,
+                    year: 1,
+                }
+            }
+        ).limit(20).toArray();
+
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        return res.end(JSON.stringify(movies));
     }
  
     res.statusCode = 404;
